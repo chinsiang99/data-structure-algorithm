45065ab6f69d0fe3e9faf2751595daa330768d37
Tan Chin Siang <chinsiang99@gmail.com>
2024-10-29 04:25:31 +0800

    feat: working with pattern 3

--- 01.patterns/pattern-03.js
+++ 01.patterns/pattern-03.js
@@ -5,11 +5,19 @@
 // 1 2 3 4
 // 1 2 3 4 5
 let n = 5
-for(let i = 0; i < n; i++){
+// for(let i = 0; i < n; i++){
+//     let row = ""
+//     // for(let j = 0; j < i + 1; j++){
+//     for(let j = 0; j <= i; j++){
+//         row += `${j+1} `
+//     }
+//     console.log(row)
+// }
+for(let i = 1; i <= n; i++){
     let row = ""
     // for(let j = 0; j < i + 1; j++){
-    for(let j = 0; j <= i; j++){
-        row += `${j+1} `
+    for(let j = 1; j <= i; j++){
+        row += `${j} `
     }
     console.log(row)
 }
