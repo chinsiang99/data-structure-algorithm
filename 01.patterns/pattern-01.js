3158a11939505ad55985397575dc2341dcd62b75
Tan Chin Siang <chinsiang99@gmail.com>
2024-10-29 04:21:05 +0800

    feat: working with pattern 1

---
+++ 01.patterns/pattern-01.js
@@ -0,0 +1,13 @@
+// this will output
+// * * * *
+// * * * *
+// * * * *
+// * * * *
+let n = 4
+for (let i = 0; i < n; i++) {
+    let row = "";
+    for (let j = 0; j < n; j++) {
+      row += "* ";
+    }
+    console.log(row);
+}
