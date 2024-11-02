f8ec0c28b386023203c4a714becc87766de5c759
Tan Chin Siang <chinsiang99@gmail.com>
2024-11-02 22:11:02 +0800

    feat: working with gcd/hcf

--- 02.basic-maths/basic-07.js
+++ 02.basic-maths/basic-07.js
@@ -16,3 +16,18 @@ function method(a, b){
 console.log(method(a, b))
 
 // note that method has a time complexity of n
+
+function method2(a, b){
+    let returnValue
+    for(let i = Math.min(a, b); i >= 1; i--){
+        if(a % i === 0 && b % i === 0){
+            returnValue = i
+            break;
+        }
+    }
+    return returnValue
+}
+
+console.log(method2(a, b))
+
+// note that this method2 is slightly better because of we want to get the greatest common divisor, so it is better to count from the last, although it is still having time complexity of n
