9e6471bf92d68b1a3ff870db4412ab8ea1bd2fe6
Tan Chin Siang <chinsiang99@gmail.com>
2024-11-02 13:15:26 +0800

    feat: working with prime number more efficient way

--- 02.basic-maths/basic-06.js
+++ 02.basic-maths/basic-06.js
@@ -19,3 +19,23 @@ console.log(isPrimeNumber(n3))
 console.log(isPrimeNumber(n4))
 
 // note that this has a time complexity of n
+
+// now we can observe something here
+// for the last section, if we are getting the divisor, we can use sqrt, so we can reduce the time complexity
+
+function isPrimeNumber2(n){
+    for(let i = 2; i*i <= n; i++){
+        if(n % i === 0){
+            return false
+        }
+    }
+
+    return true
+}
+
+console.log(isPrimeNumber2(n))
+console.log(isPrimeNumber2(n2))
+console.log(isPrimeNumber2(n3))
+console.log(isPrimeNumber2(n4))
+
+// note that the time complexity is actually now sqrt(n)
