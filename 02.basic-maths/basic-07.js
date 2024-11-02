7287b5eb2472290ce75b89b2e1c266ac0483b8d9
Tan Chin Siang <chinsiang99@gmail.com>
2024-11-02 22:08:14 +0800

    feat: working with gcd/hcf

---
+++ 02.basic-maths/basic-07.js
@@ -0,0 +1,18 @@
+// gcd (greatest common divisor) / hcf (highest common factor)
+
+let a = 9
+let b = 12
+
+function method(a, b){
+    let returnValue
+    for(let i = 1; i <= Math.min(a, b); i++){
+        if(a % i === 0 && b % i === 0){
+            returnValue = i
+        }
+    }
+    return returnValue
+}
+
+console.log(method(a, b))
+
+// note that method has a time complexity of n
