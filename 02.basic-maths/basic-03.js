22e8e5b67e356539875bff11baf47e0f7d598a01
Tan Chin Siang <chinsiang99@gmail.com>
2024-11-02 12:02:15 +0800

    feat: working with palindrome

---
+++ 02.basic-maths/basic-03.js
@@ -0,0 +1,21 @@
+// palindrome problem
+// note that palindrome are the numbers for which reverse is exactly the same as the original number
+let n = 1234
+let n2 = 121
+let n3 = 2222
+let n4 = 992837
+function method(n){
+    let originalValue = n
+    let reverseNumber = 0
+    while(n > 0){
+        let remainderValue = n % 10
+        n = Math.floor(n / 10)
+        reverseNumber = reverseNumber * 10 + remainderValue
+    }
+    return reverseNumber === originalValue
+}
+
+console.log(method(n))
+console.log(method(n2))
+console.log(method(n3))
+console.log(method(n4))
