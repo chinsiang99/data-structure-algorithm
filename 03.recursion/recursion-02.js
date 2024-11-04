1d50b637ed3fb292f0041511ff2fd5ccd0686b97
Tan Chin Siang <chinsiang99@gmail.com>
2024-11-04 15:38:45 +0800

    feat: working with summation with function recursive way

--- 03.recursion/recursion-02.js
+++ 03.recursion/recursion-02.js
@@ -1,5 +1,8 @@
 let n = 5
 // problem 1 => summation of n numbers
+
+// problem 1a is actually parameterize way
+console.log("problem 1a here....")
 function problem1a(n, sum){
     if(n < 1){
         console.log(sum)
@@ -10,3 +13,16 @@ function problem1a(n, sum){
 
 // time complexity is O(n), space complexity is O(n)
 problem1a(n, 0)
+
+// problem 1b
+// problem 1b is actually using function way
+console.log("problem 1b here...")
+function problem1b(n){
+    if(n === 0){
+        return 0
+    }
+    return n + problem1b(n-1)
+}
+
+console.log(problem1b(n))
+
