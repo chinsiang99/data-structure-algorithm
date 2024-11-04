734eab7542b0618a0f9ad8bcd52de4ddf0bd4e45
Tan Chin Siang <chinsiang99@gmail.com>
2024-11-04 15:34:22 +0800

    feat: working with summation of number

---
+++ 03.recursion/recursion-02.js
@@ -0,0 +1,11 @@
+let n = 5
+// problem 1 => summation of n numbers
+function problem1a(n, sum){
+    if(n < 1){
+        console.log(sum)
+        return
+    }
+    problem1a(n-1, sum + n)
+}
+
+problem1a(n, 0)
