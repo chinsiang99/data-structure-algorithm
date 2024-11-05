2ca2240304902ed06e1d2f3918efefeb43757b45
Tan Chin Siang <chinsiang99@gmail.com>
2024-11-05 10:52:45 +0800

    feat: working with more efficient bubble sorting

--- 05.sorting/sorting-02.js
+++ 05.sorting/sorting-02.js
@@ -3,10 +3,15 @@
 let newArray = [1, 5, 6, 3, 9, 2, 3, 1, 7, 6]
 function bubbleSortAsc(arr){
     for(let i = 0; i < arr.length - 1; i++){
+        let count = 0
         for(let j = 0; j < arr.length - 1 - i; j++){
             if(arr[j] > arr[j + 1]){
                 [arr[j], arr[j+1]] = [arr[j+1], arr[j]]
+                count++
             }
+        }
+        if(count === 0){
+            break;
         }
     }   
 }
@@ -16,10 +21,15 @@ console.log(newArray)
 
 function bubbleSortDesc(arr){
     for(let i = 0; i < arr.length - 1; i++){
+        let count = 0
         for(let j = 0; j < arr.length - 1 - i; j++){
             if(arr[j] < arr[j+1]){
                 [arr[j], arr[j+1]] = [arr[j+1], arr[j]]
+                count++
             }
+        }
+        if(count === 0){
+            break;
         }
     }
 }
