3cdef89078e752c19e3e662a332c58d830c3e1ff
Tan Chin Siang <chinsiang99@gmail.com>
2024-11-05 10:44:45 +0800

    feat: working with bubble sort ascending or descending

--- 05.sorting/sorting-02.js
+++ 05.sorting/sorting-02.js
@@ -1,1 +1,28 @@
 // bubble sort
+
+let newArray = [1, 5, 6, 3, 9, 2, 3, 1, 7, 6]
+function bubbleSortAsc(arr){
+    for(let i = 0; i < arr.length - 1; i++){
+        for(let j = 0; j < arr.length - 1 - i; j++){
+            if(arr[j] > arr[j + 1]){
+                [arr[j], arr[j+1]] = [arr[j+1], arr[j]]
+            }
+        }
+    }   
+}
+
+bubbleSortAsc(newArray)
+console.log(newArray)
+
+function bubbleSortDesc(arr){
+    for(let i = 0; i < arr.length - 1; i++){
+        for(let j = 0; j < arr.length - 1 - i; j++){
+            if(arr[j] < arr[j+1]){
+                [arr[j], arr[j+1]] = [arr[j+1], arr[j]]
+            }
+        }
+    }
+}
+
+bubbleSortDesc(newArray)
+console.log(newArray)
