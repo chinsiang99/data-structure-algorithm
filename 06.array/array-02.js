a1f816a3335b2ae4907c476fe2e555d9edbf0a91
Tan Chin Siang <chinsiang99@gmail.com>
2024-11-06 16:18:02 +0800

    feat: working with optimal approach with second largest number in an array

--- 06.array/array-02.js
+++ 06.array/array-02.js
@@ -15,16 +15,32 @@ for(let i = arrayNumbers.length - 2; i >=0; i--){
 // better approach (time complexity n + n)
 let arrayNumbers2 = [1, 2, 3, 3, 2, 5, 6, 3, 8, 3, 5]
 let largest = -1
-for(let i = 0; i < arrayNumbers2.length - 1; i++){
+for(let i = 0; i < arrayNumbers2.length; i++){
     if(arrayNumbers2[i] > largest){
         largest = arrayNumbers2[i]
     }
 }
 let secondLargest = -1
-for(let i = 0; i < arrayNumbers2.length - 1; i++){
+for(let i = 0; i < arrayNumbers2.length; i++){
     if(arrayNumbers2[i] > secondLargest && arrayNumbers2[i] < largest){
         secondLargest = arrayNumbers2[i]
     }
 }
 
 console.log(secondLargest)
+
+// optimal/best approach (it took n time complexity only instead of 2n)
+
+let arrayNumbers3 = [6, 6, 6, 6, 2, 4, 5, 2, 8, 6, 4, 7]
+
+let highest = arrayNumbers3[0]
+let secondHighest = -1
+for(let i = 1; i < arrayNumbers3.length; i++){
+    if(arrayNumbers3[i] > highest){
+        secondHighest = highest
+        highest = arrayNumbers3[i]
+    }else if(arrayNumbers3[i] > secondHighest && arrayNumbers3[i] < highest){
+        secondHighest = arrayNumbers3[i]
+    }
+}
+console.log(secondHighest)
