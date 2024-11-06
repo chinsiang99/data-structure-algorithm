62378fec39b14d3a61c5fe876421bc9531e2f043
Tan Chin Siang <chinsiang99@gmail.com>
2024-11-06 11:09:30 +0800

    feat: working with merge sort ascending

---
+++ 05.sorting/sorting-04.js
@@ -0,0 +1,64 @@
+// merge sort (sorting algo)
+// divide and conquer method
+// have a better time complexity
+
+// below is sample mindset
+// arr, 0, 0, 1
+// 0, 3
+// 1, 3, 2, 5
+
+// arr, 0, 1  arr, 2, 3
+// 1, 3       2, 5
+
+// arr, 0, 0      arr, 1, 1      arr, 2, 2   arr, 3, 3
+// 1              3              2,          5
+
+
+let arrayNumbers = [1, 3, 2, 5]
+let arrayNumbers2 = [6, 3, 2, 1, 3, 4, 5, 2]
+function sortAsc(arr, low, high){
+    if(low === high){
+        return
+    }
+    let middle = Math.floor((low + high) / 2)
+    sortAsc(arr, low, middle) 
+    sortAsc(arr, middle + 1, high) 
+    merge(arr, low, middle, high)
+}
+
+function merge(arr, low, mid, high){
+    let leftIndex = low
+    let rightFirstIndex = mid + 1
+    let temporaryArray = []
+    console.log("yoyoyo", leftIndex, rightFirstIndex)
+    while(leftIndex <= mid && rightFirstIndex <= high){
+        if(arr[leftIndex] <= arr[rightFirstIndex]){
+            temporaryArray.push(arr[leftIndex])
+            leftIndex++
+        }else{
+            temporaryArray.push(arr[rightFirstIndex])
+            rightFirstIndex++
+        }
+    }
+
+    while(leftIndex <= mid){
+        temporaryArray.push(arr[leftIndex])
+        leftIndex++
+    }
+
+    while(rightFirstIndex <= high){
+        temporaryArray.push(arr[rightFirstIndex])
+        rightFirstIndex++
+    }
+
+    for(let i = low; i <= high; i++){
+        arr[i] = temporaryArray[i - low]
+    }
+}
+
+sortAsc(arrayNumbers, 0, arrayNumbers.length - 1)
+sortAsc(arrayNumbers2, 0, arrayNumbers2.length - 1)
+
+console.log(arrayNumbers)
+console.log(arrayNumbers2)
+
