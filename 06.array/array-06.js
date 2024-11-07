1a2a1c94c240499542504f2c7ac78fe8e1784250
Tan Chin Siang <chinsiang99@gmail.com>
2024-11-07 14:11:11 +0800

    feat: working with left rotate by n palces brute force

--- 06.array/array-06.js
+++ 06.array/array-06.js
@@ -8,3 +8,25 @@ for(let i = 0; i < arrayNumbers.length - 1; i++){
 }
 
 console.log(arrayNumbers)
+
+// brute force solution to rotate the array by n
+let arrayNumbers2 = [1, 2, 3, 4, 5]
+function leftRotateByNPlaces(arr, n){
+    let realRotateValue = n % arr.length
+    let temp = []
+    for(let i = 0; i < realRotateValue; i++){
+        temp.push(arr[i])
+    }
+
+    for(let i = realRotateValue; i < arr.length; i++){
+        arr[i-realRotateValue] = arr[i]
+    }
+
+    for(let i = arr.length - realRotateValue; i < arr.length; i++){
+        arr[i] = temp[i - realRotateValue - 1]
+    }
+}
+
+console.log("original", arrayNumbers2)
+leftRotateByNPlaces(arrayNumbers2, 2)
+console.log("rotated", arrayNumbers2)
