3183f86c32413e9f5a489f34c8f228084c78d1cf
Tan Chin Siang <chinsiang99@gmail.com>
2024-11-07 14:52:36 +0800

    feat: working with left rotate by n places optimal space

--- 06.array/array-06.js
+++ 06.array/array-06.js
@@ -33,3 +33,25 @@ function leftRotateByNPlaces(arr, n){
 console.log("original", arrayNumbers2)
 leftRotateByNPlaces(arrayNumbers2, 2)
 console.log("rotated", arrayNumbers2)
+
+let arrayNumbers3 = [1, 2, 3, 4, 5]
+// the method of it is actually doing with reversing
+function leftRotateByNPlacesOptimal(arr, n){
+    let rotateRealValue = n % arr.length
+    reverseNumber(arr, 0, rotateRealValue - 1)
+    reverseNumber(arr, rotateRealValue, arr.length - 1)
+    reverseNumber(arr, 0, arr.length - 1)
+}
+
+function reverseNumber(arr, first, last){
+    while(first < last && last > first){
+        [arr[first], arr[last]] = [arr[last], arr[first]]
+        first++
+        last--
+    }
+}
+
+// time complexity of this is actually O(2n) slightly higher than before but O(1) for space complexity
+console.log("original 2", arrayNumbers3)
+leftRotateByNPlacesOptimal(arrayNumbers3, 2)
+console.log("rotated 2", arrayNumbers3)
