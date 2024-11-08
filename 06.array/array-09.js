f6c07ccdb6269e27c0991ee6ca7379438a8409c2
Tan Chin Siang <chinsiang99@gmail.com>
2024-11-08 14:19:04 +0800

    feat: working with union optimal way

--- 06.array/array-09.js
+++ 06.array/array-09.js
@@ -19,3 +19,41 @@ function union(arr1, arr2){
 
 // time complexity is O(n1 + n2 + n1 union n2) => can say it is O(n) and the space complexity is O(n1 union n2)
 console.log(union(arr1, arr2))
+
+// better approach, two pointer
+function unionOptimal(arr1, arr2){
+    let union = []
+    let i = 0; j = 0
+    let previousElement
+    while(i < arr1.length && j < arr2.length){
+        if(arr1[i] <= arr2[j]){
+            if(arr1[i] !== union[union.length - 1]){
+                union.push(arr1[i])
+            }
+            i++
+        }else{
+            if(arr2[j] !== union[union.length - 1]){
+                union.push(arr2[j])
+            }
+            j++
+        }
+    }
+
+    while(i < arr1.length){
+        if(arr1[i] !== union[union.length - 1]){
+            union.push(arr1[i])
+        }
+        i++
+    }
+
+    while(j < arr2.length){
+        if(arr2[j] !== union[union.length - 1]){
+            union.push(arr2[j])
+        }
+        j++
+    }
+
+    return union
+}
+
+console.log(unionOptimal(arr1, arr2))
