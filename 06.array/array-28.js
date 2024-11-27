1a15c11a75746786fed20b4ece9b475999a6edd2
Tan Chin Siang <chinsiang99@gmail.com>
2024-11-28 07:15:33 +0800

    feat: todo working with pascal triangle

---
+++ 06.array/array-28.js
@@ -0,0 +1,34 @@
+// TODO: please refer to pascal triangle from take u forward .....
+// not doing this first, because this is a methematical problem...
+// before interview please do it...
+/*
+ * Pascal's Triangle:
+ * A triangular array of numbers where each number is the sum of the two numbers directly above it.
+ * The triangle starts with a single 1 at the top (row 0).
+ * Example:
+ *                1
+ *              1   1
+ *            1   2   1
+ *          1   3   3   1
+ *        1   4   6   4   1
+ *      1   5  10  10   5   1
+ *
+ * Properties:
+ * 1. Each row starts and ends with 1.
+ * 2. The n-th row corresponds to the coefficients of the binomial expansion (a + b)^(n-1).
+ * 3. Symmetry: Each row is symmetric about its center.
+ * 4. The sum of all elements in the n-th row is 2^(n-1).
+ * 5. The k-th element in the n-th row can be calculated using:
+ *      C(n, k) = n! / (k! * (n - k)!)
+ */
+
+// there basically will have three types of interview questions can be made up
+// 1) given row & column, give the value
+// 2) given row, print out all the values (meaning one row, all column of it)
+// 3) given row and column, print out the traiangle
+
+// for this kind of questions, we need to utilize the nCr mathematics
+
+function question1(){
+
+}
