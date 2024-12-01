49a03e149ed6ca3d0e7309fd5b056935e4eae7d2
Tan Chin Siang <chinsiang99@gmail.com>
2024-12-02 07:51:31 +0800

    feat: working with 4 sum problem better approach using set

--- 06.array/array-31.js
+++ 06.array/array-31.js
@@ -12,6 +12,7 @@ function method(arr, target){
                     if(arr[i] + arr[j] + arr[k] + arr[l] === target){
                         const forplet = [arr[i], arr[j], arr[k], arr[l]].sort((a,b)=>a-b).join(',')
                         answer.add(forplet)
+
                     }
                 }
             }
@@ -24,3 +25,27 @@ function method(arr, target){
 }
 // time complexity is O(n^4), time complexity is O(n)
 console.log(method(arr, target))
+
+function method2(arr, target){
+    let answer = new Set()
+    for(let i = 0; i < arr.length; i++){
+        for(let j = i+1; j < arr.length; j++){
+            let set = new Set()
+            for(let k = j+1; k < arr.length; k++){
+                let forthElement = target - arr[i] - arr[j] - arr[k]
+                if(set.has(forthElement)){
+                    let forplet = [arr[i], arr[j], arr[k], forthElement].sort((a, b)=>a-b).join(',')
+                    answer.add(forplet)
+                }
+                set.add(arr[k])
+            }
+        }
+    }
+
+    return Array.from(answer).map(arr=>{
+        return arr.split(',').map(Number)
+    })
+}
+
+// time complexity is O(n^3), space complexity is O(n)
+console.log(method2(arr, target))
