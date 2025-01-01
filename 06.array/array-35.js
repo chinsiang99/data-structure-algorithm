801bb01946a5c345b2e5fda29c54da86555412ef
Tan Chin Siang <chinsiang99@gmail.com>
2025-01-01 10:01:44 +0800

    feat: better approch using map

--- 06.array/array-35.js
+++ 06.array/array-35.js
@@ -28,4 +28,34 @@ function method1(arr, n){
     return [repeating, missing]
 }
 
+// time complexity is O(n^2), space complexity is O(1)
 console.log(method1(arr, n))
+
+// using mapping approach
+function method2(arr, n){
+    let frequencyMap = new Map();
+    let repeating = -1
+    let missing = -1
+    for (let i = 1; i <= n; i++) {
+      frequencyMap.set(i, 0);
+    }
+
+    for(let i = 0; i < n; i++){
+        frequencyMap.set(arr[i], frequencyMap.get(arr[i]) + 1)
+    }
+
+    for(let [key, value] of frequencyMap){
+        if(value === 0){
+            missing = key
+        }
+
+        if(value > 1){
+            repeating = key
+        }
+    }
+
+    return [repeating, missing]
+}
+
+// time complexity is O(n), space complexity is O(n)
+console.log(method2(arr, n))
