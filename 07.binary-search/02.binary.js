93b2a91688016c8496dd27cc679114fedcd20635
Tan Chin Siang <chinsiang99@gmail.com>
2025-01-07 16:55:41 +0800

    feat: working with binary search to solve lower bound problem

--- 07.binary-search/02.binary.js
+++ 07.binary-search/02.binary.js
@@ -2,7 +2,7 @@
 // the smallest index such that the number of that index is greater than or equal to given number
 
 let arr = [3, 5, 8, 15, 19]
-let target = 20
+let target = 4
 
 function method1(arr, target){
     let answer = arr.length
@@ -18,4 +18,27 @@ function method1(arr, target){
 
 // time complexity is O(n)
 console.log(method1(arr, target))
+
+target = 20
+function method2(arr, target){
+
+    let low = 0
+    let high = arr.length - 1
+    let answer = arr.length
+    
+    while(low <= high){
+        let mid = Math.floor((low + high) / 2)
+        if(arr[mid] >= target){
+            answer = mid
+            high = mid - 1
+        }else if(arr[mid] < target){
+            low = mid + 1
+        }
+    }
+
+    return answer
+}
+
+// time complexity is O(n)
+console.log(method2(arr, target))
 
