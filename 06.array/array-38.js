5deaccc46b9a14f79f30e266206954bed33c2924
Tan Chin Siang <chinsiang99@gmail.com>
2025-01-07 08:48:38 +0800

    feat: working with optimal solution to get max product

--- 06.array/array-38.js
+++ 06.array/array-38.js
@@ -26,7 +26,61 @@ function method1(arr) {
     return [maximumValue, maximumProductArray];
 }
 
+// time complexity O(n^2)
 console.log(method1(arr));
+
+// optimal solution
+function method2(arr){
+    let max = -Infinity
+
+    // For prefix traversal
+    let prefix = 1;
+    let tempStartPrefix = 0;
+    let startPrefix = 0, endPrefix = 0;
+
+    // For suffix traversal
+    let suffix = 1;
+    let tempStartSuffix = arr.length - 1;
+    let startSuffix = 0, endSuffix = 0;
+
+    let start = 0, end = 0
+
+    for(let i = 0; i < arr.length; i++){
+        // prefix traversal
+        if(prefix === 0){
+            prefix = 1
+            tempStartPrefix = i
+        }
+        prefix = prefix * arr[i]
+        if(prefix > max){
+            max = prefix
+            startPrefix = tempStartPrefix
+            endPrefix = i
+            start = startPrefix
+            end = endPrefix
+        }
+
+        if(suffix === 0){
+            suffix = 1
+            tempStartSuffix = arr.length - 1 - i
+        }
+        suffix = suffix * arr[arr.length - 1 - i]
+        if(suffix > max){
+            max = suffix
+            startSuffix = arr.length - 1 - i
+            endSuffix = tempStartSuffix
+            start = startSuffix
+            end = endSuffix
+        }
+
+        // max = Math.max(max, Math.max(prefix, suffix))
+    }
+
+    return [max, arr.slice(start, end+1)]
+}
+
+// this method will have a time complexity of O(n) and with O(1) of space complexity
+console.log(method2(arr))
 
 
 
