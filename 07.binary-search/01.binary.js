f624cc7614f684590e317f1bc120a273cfed5059
Tan Chin Siang <chinsiang99@gmail.com>
2025-01-07 16:13:14 +0800

    feat: working with binary search recursive way

--- 07.binary-search/01.binary.js
+++ 07.binary-search/01.binary.js
@@ -22,5 +22,24 @@ function method1(arr, target){
     return -1
 }
 
-// time complexity is O(logn)
+// time complexity is O(logn), space complexity O(1)
 console.log(method1(arr, target))
+
+// recursive method, try to implement yourself
+function method2(arr, low, high, target){
+    if(low > high){
+        return -1
+    }
+
+    let mid = Math.floor((low + high) / 2)
+    if(arr[mid] === target){
+        return mid
+    }else if(arr[mid] > target){
+        return method2(arr, low, mid - 1, target)
+    }else{
+        return method2(arr, mid + 1, high, target)
+    }
+}
+
+// time complexity O(logn), space complexity O(logn)
+console.log(method2(arr, 0, arr.length - 1, target))
