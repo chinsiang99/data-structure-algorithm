ea046b0dc1b5c9cc2acaf0e3d195ebced9a10594
Tan Chin Siang <chinsiang99@gmail.com>
2025-02-03 07:51:11 +0800

    feat: implement binary search to solve the problem

--- 07.binary-search/03.binary.js
+++ 07.binary-search/03.binary.js
@@ -1,8 +1,9 @@
 // problem, find the first and last occurance of x
+// note that the array is sorted
 
-let arr = [2, 4, 6, 8, 8, 8, 11, 13]
+let arr = [2, 4, 6, 8, 8, 8, 11, 13, 13]
 
-let target = 8
+let target = 13
 function method1(arr, target){
     let first = -1, last = -1
 
@@ -20,4 +21,49 @@ function method1(arr, target){
     return [first, last]
 }
 
+// time complexity is O(n)
 console.log(method1(arr, target))
+
+function method2(arr, target){
+    // implement lower bound and upper bound to get the index
+    let first = -1
+    let last = -1
+
+    let low = 0
+    let high = arr.length - 1
+
+    // implement lower bound
+    while(low <= high){
+        let mid = Math.floor((low + high) / 2)
+        if(arr[mid] >= target){
+            first = mid
+            high = mid - 1
+        }else{
+            low = mid + 1
+        }
+    }
+
+    if(first === -1 || arr[first] !== target){
+        return [-1, -1]
+    }
+
+    // implement upper bound (this is different because we are grabbing the one that is same only)
+    low = 0
+    high = arr.length - 1
+    while(low <= high){
+        let mid = Math.floor((low + high) / 2)
+        if(arr[mid] > target){
+            high = mid - 1
+        }else if(arr[mid] === target) {
+            last = mid
+            low = mid + 1
+        }else{
+            low = mid + 1
+        }
+    }
+
+    return [first, last]
+}
+
+// time complexity O(2 * logn)
+console.log(method2(arr, target))
