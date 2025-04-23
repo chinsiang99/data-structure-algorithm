6e0ed7f3fa4b37b50bb37d983ab7e5a2c9c77714
Tan Chin Siang <chinsiang99@gmail.com>
2025-04-23 17:04:45 +0800

    feat: revision pattern 14

--- 01.patterns/pattern-14.js
+++ 01.patterns/pattern-14.js
@@ -16,3 +16,22 @@ for(let i = 0; i < n; i++){
     }
     console.log(print)
 }
+
+
+console.log("--- revision starts here ---")
+
+// A 
+// A B
+// A B C
+// A B C D
+// A B C D E
+
+let k = 5
+for(let i = 0; i < k; i++){
+    // note that charcode A is 65
+    let row = ""
+    for(let j = 0; j < i + 1; j++){
+        row += `${String.fromCharCode(65 + j)} `
+    }
+    console.log(row)
+}
