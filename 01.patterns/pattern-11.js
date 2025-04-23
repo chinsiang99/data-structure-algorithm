3073a6bd95bcca08b4ad44aa3b6da0eca29f381c
Tan Chin Siang <chinsiang99@gmail.com>
2025-04-23 16:26:15 +0800

    feat: revision pattern 11

--- 01.patterns/pattern-11.js
+++ 01.patterns/pattern-11.js
@@ -22,3 +22,28 @@ for(let i = 1; i <=n ; i++){
 
     console.log(row)
 }
+
+console.log("--- revision starts here ---")
+
+// 0 
+// 1 0
+// 0 1 0
+// 1 0 1 0
+// 0 1 0 1 0
+
+let k = 5
+for(let i = 0; i < k; i++){
+    let row = ""
+    let start = ""
+    if(i % 2 === 0){
+        start = 0
+    }else{
+        start = 1
+    }
+
+    for(let j = 0; j < i + 1; j++){
+        row += `${start % 2} `
+        start++
+    }
+    console.log(row)
+}
