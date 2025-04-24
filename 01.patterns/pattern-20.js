45b17af45d343dc68ea573eb0d1c8db7981cbf7a
Tan Chin Siang <chinsiang99@gmail.com>
2025-04-24 08:24:25 +0800

    feat: revision pattern 20

--- 01.patterns/pattern-20.js
+++ 01.patterns/pattern-20.js
@@ -36,3 +36,31 @@ for(let i = 0; i < 2 * n - 1; i++){
         console.log(stars + spaces + spaces + stars)
     }
 }
+
+console.log("--- revision starts here ---")
+
+// *        *
+// **      **
+// ***    ***
+// ****  ****
+// **********
+// ****  ****
+// ***    ***
+// **      **
+// *        *
+
+let k = 5
+
+for(let i = 0; i < 2 * k - 1; i++){
+    let stars = ""
+    for(let j = 0; i < k ? j < i + 1 : j < 2 * k - i - 1; j++){
+        stars += "*"
+    }
+
+    let space = ""
+    for(let j = 0; i < k ? j < k - i - 1 : j < i - k + 1; j++){
+        space += " "
+    }
+
+    console.log(stars + space + space + stars)
+}
