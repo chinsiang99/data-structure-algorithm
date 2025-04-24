7956eadb7134a20638a265f74e37e4f6f7ec6658
Tan Chin Siang <chinsiang99@gmail.com>
2025-04-24 08:39:55 +0800

    feat: revision pattern 21

--- 01.patterns/pattern-21.js
+++ 01.patterns/pattern-21.js
@@ -9,14 +9,19 @@ let n = 5
 for(let i = 0; i < n; i++){
     let stars = ""
     for(let j = 0; j < n; j++){
-        if(i === 0 || i === n - 1){
+        // if(i === 0 || i === n - 1){
+        //     stars += "*"
+        // }else{
+        //     if(j === 0 || j === n - 1){
+        //         stars += "*"
+        //     }else{
+        //         stars += " "
+        //     }
+        // }
+        if(i === 0 || i === n - 1 || j === 0 || j === n - 1){
             stars += "*"
         }else{
-            if(j === 0 || j === n - 1){
-                stars += "*"
-            }else{
-                stars += " "
-            }
+            stars += " "
         }
     }
     console.log(stars)
