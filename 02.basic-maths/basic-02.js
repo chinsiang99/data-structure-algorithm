580cc07a925fe2708fa149da774df093b865f154
Tan Chin Siang <chinsiang99@gmail.com>
2025-04-25 13:52:56 +0800

    feat: revision basic maths 2

--- 02.basic-maths/basic-02.js
+++ 02.basic-maths/basic-02.js
@@ -14,3 +14,39 @@ function method(n){
 console.log(method(n))
 
 // note that the time complexity is actually log base 10 n
+
+
+
+console.log("--- revision starts here ---")
+
+
+let k = 8896
+
+let reversedNumber = 0
+let trailingNumberZero = true
+while(k > 0){
+    let lastDigit = k % 10
+    if(lastDigit === 0){
+        if(!trailingNumberZero){
+            reversedNumber = reversedNumber * 10 + lastDigit
+        }
+    }else{
+        trailingNumberZero = false
+        reversedNumber = reversedNumber * 10 + lastDigit
+    }
+
+    k = Math.floor(k/10)
+}
+
+// it is actually time complexity O(log10(n)), but this one is not efficient enough, because if it is 0 first at last digit, it will always get 0, so we shouldnt do something like this, below is a good example of it
+console.log(reversedNumber)
+
+let m = 40100
+let result = 0
+while(m > 0){
+    let lastDigit = m % 10
+    result = result * 10 + lastDigit
+    m = Math.floor(m / 10)
+}
+
+console.log(result)
