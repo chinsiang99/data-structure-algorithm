42dda8081db1ed21e0d329051e9c707905be92aa
Tan Chin Siang <chinsiang99@gmail.com>
2025-05-02 01:30:08 +0800

    feat: revision basic hashing 1

--- 04.hashing/hashing-01.js
+++ 04.hashing/hashing-01.js
@@ -13,3 +13,36 @@ function method1(arr){
 }
 
 console.log(method1(arr))
+
+
+console.log("--- revision starts here ---")
+
+
+arr = [1, 2, 3, 4, 5, 1, 2, 3]
+function method(arr){
+    let newArray = Array(arr.length).fill(0)
+
+    for(let i = 0; i < arr.length; i++){
+        let number = arr[i]
+        newArray[number - 1] = newArray[number - 1] + 1
+    }
+
+    return newArray
+}
+
+console.log(method(arr))
+
+function method2(arr){
+    let newMap = new Map()
+    for(let i = 0; i < arr.length; i++){
+        if(newMap.has(arr[i])){
+            newMap.set(arr[i], newMap.get(arr[i]) + 1)
+        }else{
+            newMap.set(arr[i], 1)
+        }
+    }
+
+    return newMap
+}
+
+console.log(method2(arr))
