f45f654450cfba6e47bd5b174e8ea0da3110c62b
Tan Chin Siang <chinsiang99@gmail.com>
2025-05-02 02:50:27 +0800

    feat: revision on bubble sort

--- 05.sorting/sorting-02.js
+++ 05.sorting/sorting-02.js
@@ -38,3 +38,32 @@ bubbleSortDesc(newArray)
 console.log(newArray)
 
 // both time complexity is O(n^2)
+
+
+
+console.log("--- revision starts here ---")
+
+newArray = [1, 5, 6, 3, 9, 2, 3, 1, 7, 6]
+function bubbleSort(arr){
+    for(let i = 0; i < arr.length; i++){
+        let swapped = false
+
+        // note that - i is important, because we will know that the largest one will always been swapped
+        for(let j = 0; j < arr.length - 1; j++){
+            if(arr[j] > arr[j + 1]){
+                let temp = arr[j]
+                arr[j] = arr[j + 1]
+                arr[j + 1] = temp
+                swapped = true
+            }
+        }
+
+        if(!swapped){
+            break
+        }
+    }
+
+    console.log(arr)
+}
+
+bubbleSort(newArray)
