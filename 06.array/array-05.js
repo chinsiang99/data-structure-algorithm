73d19b0b6072065d08de069dba0f387c40a0e0fe
Tan Chin Siang <chinsiang99@gmail.com>
2025-05-06 14:55:25 +0800

    feat: revision array 5

--- 06.array/array-05.js
+++ 06.array/array-05.js
@@ -19,7 +19,7 @@ for(let i = 1; i < arrayNumbers.length; i++){
 console.log(uniqueIndex + 1)
 
 // removing duplicates, but only show them infront, others all showing 0
-let arrayNumbers2 = [1, 1, 2, 3, 4, 4, 5, 6, 6]
+let arrayNumbers2 = [1, 2, 3, 4, 4, 5, 6, 6]
 let uniqueIndex2 = 0
 for(let i = 1; i < arrayNumbers2.length; i++){
     if(arrayNumbers2[i] !== arrayNumbers2[uniqueIndex2]){
@@ -28,4 +28,36 @@ for(let i = 1; i < arrayNumbers2.length; i++){
     }
     arrayNumbers2[i] = 0
 }
+console.log(arrayNumbers2, "helo?")
+
+
+console.log("--- revision starts here ---")
+
+arrayNumbers2 = [1, 1, 2, 3, 4, 4, 5, 6, 6]
+
+let unique = 0
+
+for(let i = 1; i < arrayNumbers2.length; i++){
+    if(arrayNumbers2[i] !== arrayNumbers2[unique]){
+        unique++
+        arrayNumbers2[unique] = arrayNumbers2[i]
+    }
+    arrayNumbers2[i] = 0
+}
+
 console.log(arrayNumbers2)
+
+
+removeDuplicate = [1, 1, 2, 3, 4]
+// removeDuplicate = [1, 3, 5, 7, 9]
+
+let uniqueTo = 0
+for(let j = 1; j < removeDuplicate.length; j++){
+    if(removeDuplicate[uniqueTo] !== removeDuplicate[j]){
+        uniqueTo++
+        removeDuplicate[uniqueTo] = removeDuplicate[j]
+    }
+
+}
+
+console.log(uniqueTo + 1, removeDuplicate)
