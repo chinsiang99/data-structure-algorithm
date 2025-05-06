188d3c63d01f9f03cc92717b6cb5137ddc7c4544
Tan Chin Siang <chinsiang99@gmail.com>
2025-05-06 08:53:28 +0800

    feat: revision array 3

--- 06.array/array-03.js
+++ 06.array/array-03.js
@@ -13,3 +13,38 @@ for(let i = 1; i < arrayNumbers.length; i++){
     }
 }
 console.log(secondSmallest)
+
+console.log("--- revision starts here ---")
+
+arrayNumbers = [1, 1, 2, 5, 6, 3, 5, 7, 8, 4]
+
+let smallestValue = Math.min(arrayNumbers[0], arrayNumbers[arrayNumbers.length - 1])
+let secondSmallestValue = Math.max(arrayNumbers[0], arrayNumbers[arrayNumbers.length - 1])
+if(smallestValue === secondSmallestValue){
+    secondSmallestValue = -1
+}
+let leftPointer = 1
+let rightPointer = arrayNumbers.length - 2
+while(leftPointer <= rightPointer){
+    if(arrayNumbers[leftPointer] < secondSmallestValue){
+        if(arrayNumbers[leftPointer] < smallestValue){
+            secondSmallestValue = smallestValue
+            smallestValue = arrayNumbers[leftPointer]
+        }else if(arrayNumbers[leftPointer] !== smallestValue){
+            secondSmallestValue = arrayNumbers[leftPointer]
+        }
+    }
+
+    if(arrayNumbers[rightPointer] < secondSmallestValue){
+        if(arrayNumbers[rightPointer] < smallestValue){
+            secondSmallestValue = smallestValue
+            smallestValue = arrayNumbers[rightPointer]
+        }else if(arrayNumbers[rightPointer] !== smallestValue){
+            secondSmallestValue = arrayNumbers[rightPointer]
+        }
+    }
+    leftPointer++
+    rightPointer--
+}
+
+console.log(smallestValue, secondSmallestValue)
