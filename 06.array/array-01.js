f63053bb83402f39be6b19f1229ad362a3f47261
Tan Chin Siang <chinsiang99@gmail.com>
2025-05-06 08:28:10 +0800

    feat: revision on array 1

--- 06.array/array-01.js
+++ 06.array/array-01.js
@@ -10,3 +10,31 @@ for(let i = 1; i < arrayNumbers.length - 1; i++){
     }
 }
 console.log(largest)
+
+console.log("--- revision starts here ---")
+
+// find the largest number
+
+arrayNumbers = [1, 3, 5, 7, 3, 4, 5, 8, 10, 1]
+
+let largestNumber = arrayNumbers[0]
+for(let i = 1; i < arrayNumbers.length; i++){
+    if(arrayNumbers[i] > largestNumber){
+        largestNumber = arrayNumbers[i]
+    }
+}
+
+console.log(largestNumber)
+
+let leftPointer = 1
+let rightPointer = arrayNumbers.length - 1
+let largestValue = arrayNumbers[0]
+while(leftPointer <= rightPointer){
+    if(largestValue < Math.max(arrayNumbers[leftPointer], arrayNumbers[rightPointer])){
+        largestValue = Math.max(arrayNumbers[leftPointer], arrayNumbers[rightPointer])
+    }
+    leftPointer++
+    rightPointer--
+}
+
+console.log(largestValue)
