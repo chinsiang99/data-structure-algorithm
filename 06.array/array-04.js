a544e7e56c6b0093347314d04d2a1a785a9d099a
Tan Chin Siang <chinsiang99@gmail.com>
2025-05-06 09:28:15 +0800

    feat: revision array 4

--- 06.array/array-04.js
+++ 06.array/array-04.js
@@ -20,3 +20,19 @@ for(let i = 0; i < arrayNumbers2.length - 1; i++){
     }
 }
 console.log(isSorted2)
+
+
+
+console.log("--- revision starts here ---")
+arrayNumbers = [1, 2, 3, 3, 2, 4, 6, 2, 1]
+
+let sortFlag = true
+for(let i = 0; i < arrayNumbers.length - 1; i++){
+    if(arrayNumbers[i] > arrayNumbers[i + 1]){
+        sortFlag = false
+        break;
+    }
+}
+console.log(sortFlag)
+
+let sortFlag2 = trye
