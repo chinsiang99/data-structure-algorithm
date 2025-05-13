b2d7127b8cbcaf9cce6a58902a45c0c1d4a0c078
Tan Chin Siang <chinsiang99@gmail.com>
2025-05-13 15:34:21 +0800

    feat: revision array 8

--- 06.array/array-08.js
+++ 06.array/array-08.js
@@ -16,3 +16,16 @@ function linearSearch(arr, target){
 
 console.log(linearSearch(arrayNumbers, 5))
 console.log(linearSearch(arrayNumbers, 4))
+
+console.log("--- revision starts here ---")
+
+let array = [6, 8, 3, 4, 1]
+let target = 8
+for(let i = 0; i < array.length; i++){
+    if(array[i] === target){
+        target = i
+        break
+    }
+}
+
+console.log(target)
