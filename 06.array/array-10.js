a3e4208a4d3b627d2805a3e0ed628bb89d585386
Tan Chin Siang <chinsiang99@gmail.com>
2025-05-14 08:45:42 +0800

    feat: revision array 10

--- 06.array/array-10.js
+++ 06.array/array-10.js
@@ -2,7 +2,7 @@
 // intersection meaning that present in both
 
 let arr1 = [1, 2, 2, 3, 3, 4, 5, 6]
-let arr2 = [2, 3, 3, 5, 7]
+let arr2 = [3, 3, 5, 7]
 
 // brute force way
 function intersection(arr1, arr2){
@@ -48,3 +48,71 @@ function intersectionOptimal(arr1, arr2){
 }
 // time complexity is actually n1 + n2, without additional time complexity although as an intersect
 console.log(intersectionOptimal(arr1, arr2))
+
+
+console.log("--- revision starts here ---")
+
+
+let array1 = [1, 2, 2, 3, 3, 4, 5, 6]
+let array2 = [2, 2, 2, 3, 3, 5, 7]
+let map = new Map()
+let newIntersectArray = []
+
+for(let i = 0; i < array1.length; i++){
+    if(!map.has(array1[i])){
+        map.set(array1[i], 1)
+    }else{
+        map.set(array1[i], map.get(array1[i]) + 1)
+    }
+}
+
+for(let i = 0; i < array2.length; i++){
+    if(map.has(array2[i]) && map.get(array2[i]) > 0){
+        map.set(array2[i], map.get(array2[i]) - 1)
+        newIntersectArray.push(array2[i])
+    }
+}
+
+// time complexity is actually n.length + m.length 
+// space complexity is Math.max(n.length, m.length)
+console.log(newIntersectArray)
+
+// now we can try two pointer approach
+
+let pointer1 = 0
+let pointer2 = 0
+let newIntersection = []
+while(pointer1 < array1.length && pointer2 < array2.length){
+    if(array1[pointer1] < array2[pointer2]){
+        pointer1++
+    }else if(array1[pointer1] === array2[pointer2]){
+        newIntersection.push(array1[pointer1])
+        pointer1++
+        pointer2++
+    }else{
+        pointer2++
+    }
+}
+
+// note that space compelxity is being neglected
+console.log(newIntersection)
+
+// note that below approach is when we only want to have distinct value only
+let previousElement = -1
+let newA = []
+pointer1 = 0
+pointer2 = 0
+while(pointer1 < array1.length && pointer2 < array2.length){
+    if(array1[pointer1] === array2[pointer2] && array1[pointer1] !== previousElement){
+        previousElement = array1[pointer1]
+        newA.push(array1[pointer1])
+        pointer1++
+        pointer2++
+    }else if(array1[pointer1] < array2[pointer2]){
+        pointer1++
+    }else{
+        pointer2++
+    }
+}
+
+console.log(newA)
