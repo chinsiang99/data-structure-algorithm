2b6f84b23cfe900c2dace659c721cc9fb966b90f
Tan Chin Siang <chinsiang99@gmail.com>
2025-05-16 20:28:41 +0800

    feat: revision array 12

--- 06.array/array-12.js
+++ 06.array/array-12.js
@@ -27,3 +27,23 @@ function findMaximumConsecutive(arr){
 }
 
 console.log(findMaximumConsecutive(array))
+
+console.log("--- revision starts here ---")
+
+let consecArray = [1, 1, 1, 0, 0, 0, 1, 0, 1, 1, 1, 1, 0, 0, 1]
+
+let startIndex = 0
+let maximum = 1
+for(let i = 1; i < consecArray.length; i++){
+    if(consecArray[i] !== consecArray[startIndex]){
+        maximum = Math.max(maximum, i - startIndex)
+        startIndex = i
+    }
+}
+
+maximum = Math.max(maximum, consecArray.length - startIndex - 1)
+console.log(maximum)
+
+// time complexity will be O(n)
+// space complexity will be O(1)
+// for this we will utilize two pointer approach in order to get consecutive array (same number)
