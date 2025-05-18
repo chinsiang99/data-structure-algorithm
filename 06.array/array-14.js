0775f1744091614a373d2ebc58816805ea8bcd54
Tan Chin Siang <chinsiang99@gmail.com>
2025-05-18 08:21:47 +0800

    feat: revision array 14

--- 06.array/array-14.js
+++ 06.array/array-14.js
@@ -57,3 +57,31 @@ function mapping(arr, k){
 
 // time complexity is O(n)
 console.log(mapping(array, 3))
+
+console.log("--- revision starts here ---")
+
+array = [1, 2, 3, 1, 1, 1, 1, 4, 2, 3]
+
+let mapped = new Map()
+let cumulativeNumber = 0
+let target = 1
+let length = 0
+for(let i = 0; i < array.length; i++){
+    cumulativeNumber += array[i]
+
+    if(cumulativeNumber === target){
+        length = i + 1
+    }
+
+    let diff = cumulativeNumber - target
+
+    if(mapped.has(diff)){
+        length = Math.max(length, i - mapped.get(diff))
+    }
+
+    if(!mapped.has(cumulativeNumber)){
+        mapped.set(cumulativeNumber, i)
+    }
+}
+
+console.log(length)
