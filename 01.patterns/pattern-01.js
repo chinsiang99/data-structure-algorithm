2be08f2efdb0d82ae8b325358704800a1976266b
Tan Chin Siang <chinsiang99@gmail.com>
2025-07-08 15:30:53 +0800

    revision on pattern 1

--- 01.patterns/pattern-01.js
+++ 01.patterns/pattern-01.js
@@ -25,3 +25,26 @@ for(let i = 0; i < k; i++){
 }
 
 // time complexity will be O(n^2) because there is two loop
+
+
+// final revision
+
+// this will output
+// * * * *
+// * * * *
+// * * * *
+// * * * *
+
+let star = 4
+
+for(let i = 0; i < star; i++){
+  let stars = ''
+
+  for(let j = 0; j < star; j++){
+    stars += '* '
+  }
+
+  console.log(stars)
+}
+
+
