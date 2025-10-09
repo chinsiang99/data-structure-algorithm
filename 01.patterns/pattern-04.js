97f9df6995aa1f8c3d8cbd2fbff2e1ed96e0b4aa
Tan Chin Siang <chinsiang99@gmail.com>
2025-10-09 13:26:14 +0800

    feat: pattern 4 revision

--- 01.patterns/pattern-04.js
+++ 01.patterns/pattern-04.js
@@ -54,3 +54,23 @@ for(let i = 0; i < rows; i++){
     }
     console.log(numbers)
 }
+
+
+// practise for a job application
+console.log("-----for job application-----")
+
+// 1
+// 2 2
+// 3 3 3
+// 4 4 4 4
+// 5 5 5 5 5
+
+let p = 5
+
+for(let i = 1; i <= p; i++){
+    let row = ''
+    for(let j = 0; j < i; j++){
+        row += `${i} `
+    }
+    console.log(row)
+}
