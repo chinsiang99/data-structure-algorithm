6cb3652803d76160c7ecede289d262cda911dcde
Tan Chin Siang <chinsiang99@gmail.com>
2025-11-20 03:12:52 +0800

    feat: revision array 5

--- 06.array/array-05.js
+++ 06.array/array-05.js
@@ -37,6 +37,7 @@ arrayNumbers2 = [1, 1, 2, 3, 4, 4, 5, 6, 6]
 
 let unique = 0
 
+// this solution is wrong
 for(let i = 1; i < arrayNumbers2.length; i++){
     if(arrayNumbers2[i] !== arrayNumbers2[unique]){
         unique++
@@ -61,3 +62,34 @@ for(let j = 1; j < removeDuplicate.length; j++){
 }
 
 console.log(uniqueTo + 1, removeDuplicate)
+
+
+
+// practise for a job application
+console.log("-----for job application-----")
+
+// removing duplicates in a sorted array
+let arrayNumber = [1, 1, 2, 3, 4, 5, 6, 7, 8, 8]
+
+unique = 0
+console.log(arrayNumber)
+for(let i = 1; i < arrayNumber.length; i++){
+    if(arrayNumber[i] !== arrayNumber[unique]){
+        unique++
+        if(unique !== i){
+            arrayNumber[unique] = arrayNumber[i]
+        }
+    }
+    if(i !== unique){
+        arrayNumber[i] = 0
+    }
+}
+
+console.log(arrayNumber)
+
+console.log(unique)
+// note that we can use slice to get it as well
+
+console.log(arrayNumber.slice(0,8))
+
+// console.log(arrayNumber.splice(8))
