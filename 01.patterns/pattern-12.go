46a1e6d5b4a088e81e63df8d9d5f000d9ba54842
Tan Chin Siang <chinsiang99@gmail.com>
2026-01-19 15:51:50 +0800

    feat: revision pattern 12 with go

---
+++ 01.patterns/pattern-12.go
@@ -0,0 +1,57 @@
+// ------------------ revision with js and go -------------------------
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+// 1        1
+// 12      21
+// 123    321
+// 1234  4321
+// 1234554321
+
+func main() {
+	p := 5
+
+	// // for i := 0; i < p; i++ {
+
+	// // }
+
+	// for i := 0; i < p; i++ {
+	// 	space := strings.Repeat("  ", p-i-1)
+	// 	stars := strings.Repeat("* ", 2*i+1)
+	// 	fmt.Println(space + stars)
+	// }
+
+	// for i := 0; i < 2*p-1; i++ {
+	// 	var stars string
+	// 	if i < p {
+	// 		stars = strings.Repeat("* ", i+1)
+	// 	} else {
+	// 		stars = strings.Repeat("* ", 2*p-i-1)
+	// 	}
+	// 	fmt.Println(stars)
+	// }
+
+	fmt.Println("---------")
+
+	for i := 1; i <= p; i++ {
+		var b strings.Builder
+		for j := 1; j <= i; j++ {
+			b.WriteString(strconv.Itoa(j))
+		}
+
+		// space
+		for j := 0; j < p-i; j++ {
+			b.WriteString("  ")
+		}
+
+		for j := i; j > 0; j-- {
+			b.WriteString(strconv.Itoa(j))
+		}
+		fmt.Println(b.String())
+	}
+}
