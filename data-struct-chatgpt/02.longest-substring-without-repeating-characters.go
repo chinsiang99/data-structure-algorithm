30a527ee9d19fd16bc82d6858da7b81697f2c205
Tan Chin Siang <chinsiang99@gmail.com>
2026-02-19 02:39:01 +0800

    feat: longest substring without repeating characters with go

---
+++ data-struct-chatgpt/02.longest-substring-without-repeating-characters.go
@@ -0,0 +1,43 @@
+/* Problem 2: Longest Substring Without Repeating Characters
+
+Given a string s, find the length of the longest substring (contiguous) that contains no repeated characters.
+
+Examples:
+
+"abcabcbb" → 3 ( "abc" )
+
+"bbbbb" → 1 ( "b" )
+
+"pwwkew" → 3 ( "wke" )
+
+"" → 0 */
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(longestSubstringWithoutRepeatingCharacters("abcabcbb"))
+	fmt.Println(longestSubstringWithoutRepeatingCharacters("abcadbcbb"))
+	fmt.Println(longestSubstringWithoutRepeatingCharacters("bbbbbb"))
+	fmt.Println(longestSubstringWithoutRepeatingCharacters("pwwkew"))
+	fmt.Println(longestSubstringWithoutRepeatingCharacters("a"))
+	fmt.Println(longestSubstringWithoutRepeatingCharacters("abba"))
+}
+
+func longestSubstringWithoutRepeatingCharacters(word string) int {
+	mapping := make(map[byte]int)
+	left := 0
+	longest := 0
+
+	for right := 0; right < len(word); right++ {
+		value, found := mapping[word[right]]
+		if found && value >= left {
+			left = value + 1
+		}
+		mapping[word[right]] = right
+		longest = max(longest, right-left+1)
+	}
+
+	return longest
+}
